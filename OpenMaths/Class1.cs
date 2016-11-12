2e95d0f7f37edaf45674ea114fdc942a70459b1a
alawi <alawi@LAPTOP-L22L6CC2>
2016-11-12 17:58:06 +0000

    Add project files.

---
+++ OpenMaths/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMaths
+{
+    public class Class1
+    {
+    }
+}
